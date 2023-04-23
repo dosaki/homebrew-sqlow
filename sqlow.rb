0862bafc8e0fa600efbabd41167992601c98c11f
Tiago Correia <tiago.f.a.correia@gmail.com>
2023-04-23 03:50:03 +0100

    Upgraded to 0.0.5

--- sqlow.rb
+++ sqlow.rb
@@ -1,28 +1,18 @@
 class Sqlow < Formula
-  @version = "0.0.3"
+  @@version = "0.0.5"
   desc "A CLI database schema migrator that doesn't care about versions"
   homepage "https://github.com/dosaki/sqlow"
-  url "https://github.com/dosaki/sqlow/archive/refs/tags/#{@version}.tar.gz"
-  sha256 "818c49e50e205693d507f6d27ea3f98cfa7fb2cbbb28df8f69442b1b1df86747"
+  url "https://github.com/dosaki/sqlow/archive/refs/tags/#{@@version}.tar.gz"
+  sha256 "4c657bd27b672f2c7b2b1d9ccb6478eec2d77d1fe1c283d250fb2510b1dbe7a6"
   license "Apache Licence, Version 2.0"
   head "https://github.com/dosaki/sqlow"
   
   depends_on "bash" => :build
   depends_on "go" => :build
 
-  @sys_type = "darwin"
-  @sys_arch = "arm64"
-  on_linux do
-    @sys_type = "linux"
-  end
-  on_intel do
-    @sys_arch = "amd64"
-  end
-
-
   def install
     ENV["GOPATH"] = buildpath
-    system "./build.sh", "--default", "--output=#{buildpath}/dist", "--version=#{@version}", "--no-zip"
+    system "./build.sh", "--default", "--output=#{buildpath}/dist", "--version=#{@@version}", "--no-zip"
     bin.install "#{buildpath}/dist/sqlow"
   end
 
