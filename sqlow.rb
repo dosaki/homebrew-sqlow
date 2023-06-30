973a067b82e621d611fd7243ee608056a51f85ca
Tiago Correia <tiago.f.a.correia@gmail.com>
2023-06-30 09:56:26 +0100

    sqlow v0.1.3

--- sqlow.rb
+++ sqlow.rb
@@ -1,9 +1,9 @@
 class Sqlow < Formula
-  @@version = "0.1.2"
+  @@version = "0.1.3"
   desc "A CLI database schema migrator that doesn't care about versions"
   homepage "https://github.com/dosaki/sqlow"
   url "https://github.com/dosaki/sqlow/archive/refs/tags/#{@@version}.tar.gz"
-  sha256 "a3021185f510c85b6dc2e5ff97aa9346543369b228477a774b02c80ad1551e3a"
+  sha256 "c8fe6042e33ede9886636abfe56da1db5c4a9144fec4168fd426906cf39e0853"
   license "Apache Licence, Version 2.0"
   head "https://github.com/dosaki/sqlow"
   
